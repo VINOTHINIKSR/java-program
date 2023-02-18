cff44e078aa5a6459fb9b5f3f0705b12dae5b6bc
VINOTHINIKSR <125643104+VINOTHINIKSR@users.noreply.github.com>
2023-02-18 21:24:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt(),b=sc.nextInt();
+	    if((a*a)==(b*b))
+		System.out.println("perfect number");
+		else
+			System.out.println(" not perfect number");
+	}
+}
