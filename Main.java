54eb6b2daa195d48141555ea7470d79a691ef440
VINOTHINIKSR <125643104+VINOTHINIKSR@users.noreply.github.com>
2023-02-18 19:50:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int count=0;
+	    for(int i=2;i<a;i++){
+	        if(a%i==0){
+	            count++;
+	        }
+	    }
+	    if(count==0){
+		System.out.println("Prime number");
+	}
+	else{
+	    	System.out.println("Not Prime number");
+	}
+}
+}
